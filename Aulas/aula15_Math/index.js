462ec69f85e7040b72ce8d4d93aa30e1c3200f68
Gortelan <gortelan.montanari@hotmail.com>
2024-03-31 19:11:35 -0300

    Up

---
+++ Aulas/aula15_Math/index.js
@@ -0,0 +1,19 @@
+let num1 = 9.543232;
+
+//Arredondar o valor para baixo
+// let num2 = Math.floor(num1);
+
+//Arrendodar para cima
+// let num2 = Math.ceil(num1);
+
+//Retorna um numero arrendodado para o mais proximo
+// let num2 = Math.round(num1);
+
+//Retorna o maior numero dessa sequencia
+// let num2 = Math.max(1,2,3,4,5,6,-10,-590,1005);
+
+//Retorna o menor numero dessa sequencia
+// let num2 = Math.min(1,2,3,4,5,6,-10,-590,1005);
+
+//Zera numeros aleatorios
+// console.log(Math.random());
